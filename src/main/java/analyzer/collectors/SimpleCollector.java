e1be85ef57e1f1a815b8fd902053bfa493a226d2
Grigory Panov <gripanov@gmail.com>
2018-05-12 01:14:51 +0300

    Implement simple warning collector

--- src/main/java/analyzer/collectors/SimpleCollector.java
+++ src/main/java/analyzer/collectors/SimpleCollector.java
@@ -1,31 +1,21 @@
 package analyzer.collectors;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class SimpleCollector implements Collector {
+
+    private HashMap<String, List<String>> warnings;
+
     @Override
     public void addWarning(String className, String warning) {
-
-    }
-
-    @Override
-    public void incrementMetric(String metricName) {
-
-    }
-
-    @Override
-    public void incrementMetric(String metricName, int count) {
-
+        warnings.putIfAbsent(className, new ArrayList<>());
+        warnings.get(className).add(warning);
     }
 
     @Override
     public HashMap<String, List<String>> getWarnings() {
-        return null;
-    }
-
-    @Override
-    public HashMap<String, Integer> getStats() {
-        return null;
+        return warnings;
     }
 }
